0e2114b7d0cb5b083c94fca990fb6036b387e4f5
Ezracancoj <102781998+Ezracancoj@users.noreply.github.com>
2022-11-21 16:30:29 -0500

    Add files via upload

---
+++ InterfaceV2/src/Quiz.java
@@ -0,0 +1,15 @@
+public class Quiz implements Measurable {
+	   private int testScore;
+	
+	   public Quiz(int testScore)
+	   {
+	      this.testScore = testScore;
+	   }
+
+
+	  public double getMeasure()
+	   {
+	      return testScore;
+	   }
+
+}
